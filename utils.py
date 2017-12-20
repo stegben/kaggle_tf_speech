afae8007fd7cc29d1ad011a68596070870b023fd
cph-gcp <stegben.benjamin@gmail.com>
2017-12-20 17:24:03 +0000

    augmentation

--- utils.py
+++ utils.py
@@ -1,14 +1,19 @@
 import os.path as osp
 
 import numpy as np
+from scipy.misc import imresize
 from scipy.io import wavfile
 
 from constants import TRAIN_AUDIO_PATH
+from gen_data import pad_wave_with_zero
 
+
+MEAN = -17.6725
+STD = 3128.12
 
 def read_background(name):
     path = osp.join(TRAIN_AUDIO_PATH, '_background_noise_', name)
-    wave = wavfile.read((path).astype(np.float32) / 1.0
+    wave = (wavfile.read(path).astype(np.float32) - MEAN) / STD
     return wave
 
 
@@ -20,21 +25,64 @@ exercise_bike = read_background('exercise_bike.wav')
 running_tap = read_background('running_tap.wav')
 
 
+def sample_bg(wave, length, ratio):
+    start = np.random.randint(0, (wave.shape[0] - length))
+    vol_ratio = np.random.normal(0, ratio)
+    return wave[start: start + length] * vol_ratio
+
+
 def augment(
         arr,
         shift_range=2000,
         speed_ratio=0.2,
-        volume_ratio=3,
-        white_noise=1.0,
-        pink_noise=1.0,
-        doing_the_dishes=1.0,
-        dude_miaowing=1.0,
-        exercise_bike=1.0,
-        running_tap=1.0,
+        volume_ratio=2,
+        white_noise_ratio=0.1,
+        pink_noise_ratio=0.1,
+        doing_the_dishes_ratio=0.1,
+        dude_miaowing_ratio=0.1,
+        exercise_bike_ratio=0.1,
+        running_tap_ratio=0.1,
     ):
-    # shifting =
-    volume_adjust = np.random.uniform(1/volume_ratio, 1*volume_ratio, arr.shape[0])
-    return arr
+    length = arr.shape[1]
+    # shifting
+    shifting = np.random.randint(-shift_range, shift_range)
+    arr_modified = np.roll(arr, shifting, axis=1)
+
+
+    for idx in range(arr_modified.shape[0]):
+        # speed adjust
+        stretched_length = int(length * np.random.uniform(1 - speed_ratio, 1 + speed_ratio))
+        arr_modified[idx, :] = pad_wave_with_zero(imresize(
+            arr_modified[idx, :],
+            size=(1, stretched_length),
+        ), length)
+
+        # volume
+        # volume_adjust = np.random.uniform(1/volume_ratio, 1*volume_ratio, arr.shape[0])
+        volume_adjust = np.random.uniform(1/volume_ratio, 1*volume_ratio)
+        arr_modified[idx, :] = arr_modified[idx, :] * volume_adjust
+
+        # with noises
+        white_noise_frac = sample_bg(white_noise, length, white_noise_ratio)
+        pink_noise_frac = sample_bg(pink_noise, length, pink_noise_ratio)
+        doing_the_dishes_frac = sample_bg(doing_the_dishes, length, doing_the_dishes_ratio)
+        dude_miaowing_frac = sample_bg(dude_miaowing, length, dude_miaowing_ratio)
+        exercise_bike_frac = sample_bg(exercise_bike, length, exercise_bike_ratio)
+        running_tap_frac = sample_bg(running_tap, length, running_tap_ratio)
+
+        arr_modified[idx, :] = (
+            arr_modified[idx, :]
+            + white_noise_frac
+            + pink_noise_frac
+            + doing_the_dishes_frac
+            + dude_miaowing_frac
+            + exercise_bike_frac
+            + running_tap_frac
+        )
+
+    arr_modified = arr_modified
+    return arr_modified
+
 
 class BatchGenerator(object):
 
