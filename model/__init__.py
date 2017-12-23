d5e58a2c88ea84da62e835930309c8a1bfbbf357
cph-gcp <stegben.benjamin@gmail.com>
2017-12-23 17:26:29 +0000

    add gated convolution version of wavelet network

--- model/__init__.py
+++ model/__init__.py
@@ -121,5 +121,31 @@ ALL_MODELS = {
             'dense_dropout_prob': 0.0,
         },
     ),
+    '5': (
+        lambda input_dim, output_dim: build_wavelet_1d_2d_cnn_mlp(
+            input_dim,
+            output_dim,
+            n_wavelets=16,
+            wavelet_range=[4*k for k in range(1, 9)],
+            wavelet_length=-1,
+            conv_structure=[
+                (2, 4, 1, 2, 32, 'selu'),
+                (1, 2, 1, 2, -1, 'pooling'),
+                (2, 4, 1, 2, 32, 'selu'),
+                (1, 2, 1, 2, -1, 'pooling'),
+                (4, 16, 1, 2, 64, 'selu'),
+                (2, 4, 1, 2, -1, 'pooling'),
+            ],
+            dense_structure=[
+                (512, 'selu'),
+            ],
+            should_share_wavelet=False,
+            l2_regularize=0.00001,
+        ), {
+            'wavelet_dropout_prob': 0.1,
+            'conv_dropout_prob': 0.1,
+            'dense_dropout_prob': 0.2,
+        },
+    ),
 
 }
